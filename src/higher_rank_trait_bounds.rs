a21b85af3904004be8043dceb57642f5ca8aa1b2
Andrei Silviu Dragnea <andreidragnea.dev@gmail.com>
2023-11-23 22:28:41 +0200

    cargo update + upgrade

--- src/higher_rank_trait_bounds.rs
+++ src/higher_rank_trait_bounds.rs
@@ -18,9 +18,10 @@ fn do_it(data: &(u8, u16)) -> &u8 {
 
 #[test]
 fn test() {
-    let clo = Closure {
-        data: (0, 1),
-        func: do_it,
-    };
+    let clo =
+        Closure {
+            data: (0, 1),
+            func: do_it,
+        };
     println!("{}", clo.call());
 }
