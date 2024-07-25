a7f3678ce045ff9fbe5061067900fa6252277752
Andrei Silviu Dragnea <andreidragnea.dev@gmail.com>
2024-07-25 12:52:18 +0200

    Add explicit lifetime 2 example

--- src/lifetimes.rs
+++ src/lifetimes.rs
@@ -1,5 +1,6 @@
 type FnWithLifetimes<'a> = fn(a: &'a str, b: &'a str, c: &str);
 type FnWithExplicitLifetimes<'a> = for<'b> fn(a: &'a str, b: &'a str, c: &'b str);
+type FnWithExplicitLifetimes2 = for<'a> fn(a: &'a str, b: &'a str, c: &'a str);
 
 const S: &str = "hello";
 
@@ -15,6 +16,12 @@ fn accepts_fn_with_explicit_lifetimes<'a>(f: FnWithExplicitLifetimes<'a>) {
     f(s, S, &string);
 }
 
+fn accepts_fn_with_explicit_lifetimes2(f: FnWithExplicitLifetimes2) {
+    let s = "hello";
+    let string = String::from("a");
+    f(s, S, &string);
+}
+
 fn function_with_lifetimes(a: &str, b: &str, c: &str) {
     println!("{a} {b} {c}");
 }
@@ -23,4 +30,5 @@ fn function_with_lifetimes(a: &str, b: &str, c: &str) {
 fn test() {
     accepts_fn_with_lifetimes(function_with_lifetimes);
     accepts_fn_with_explicit_lifetimes(function_with_lifetimes);
+    accepts_fn_with_explicit_lifetimes2(function_with_lifetimes);
 }
